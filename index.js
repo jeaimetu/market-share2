e02996c8066cf37b5c0c6c963143db29f85cdad0
nowhere <32817067+jeaimetu@users.noreply.github.com>
2023-03-27 09:48:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const express = require('express')
 const path = require('path')
 
 const PORT = process.env.PORT || 5001
+
+async function webScraping(url, selector) {
+    let res = [];
+    let html = await axios.get(url);
+    let $ = cheerio.load(html.data);
+  
+    for(let v of $(selector)) {   
+        res.push($(v).text());
+    }
+  
+    return res;
+}
+
+let url = 'https://ramses.kr/compare';
+let selector = 'body > div > div > div > div > table:nth-child(5) > tbody > tr:nth-child(2) > td:nth-child(1)';
+
+webScraping(url, selector).then((res) => {
+  console.log(res)
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
